a92d1bdb80db0c2cebef14a6726a219fa7a18f21
mericks <mericks@users.noreply.github.com>
2017-04-14 08:32:19 -0700

    correct type - 'process.env'

--- server/test/_before.js
+++ server/test/_before.js
@@ -1,4 +1,4 @@
-process.eng.DB_URI = 'mongodb://localhost:27017/best-brunch-test';
+process.env.DB_URI = 'mongodb://localhost:27017/best-brunch-test';
 require('../lib/connection');
 const mongoose = require('mongoose');
 
