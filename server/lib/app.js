202e817b7cab865456f9b1d11173cc07987706f2
mericks <mericks@users.noreply.github.com>
2017-04-19 14:59:16 -0700

    add auth routes and errorhandler to server app.js

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,8 +1,14 @@
 const app = require('express')();
 const morgan = require('morgan')('dev');
+const errorHandler= require('./error-handler')();
 const cors = require('cors')();
+const auth = require('./routes/auth');
+// const ensureAuth = require('./auth/ensure-auth')();
 
 app.use(morgan);
 app.use(cors);
 
+app.use('/api/auth', auth);
+
+app.use(errorHandler);
 module.exports = app;
