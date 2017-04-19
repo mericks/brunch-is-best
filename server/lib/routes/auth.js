e3df96242ec2d70d7c6c98f71e62bdd0a95f21e0
mericks <mericks@users.noreply.github.com>
2017-04-19 14:57:21 -0700

    add auth routes and and jsonwebtoken-token.js

---
+++ server/lib/routes/auth.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser').json();
+const User = require('../models/user');
+const token = require('../auth/token');
+const ensureAuth = require('../auth/ensure-auth')();
+
+function hasEmailAndPassword(req, res, next) {
+    const user = req.body;
+    if(!user.email || !user.password) {
+        return next({
+            code: 400,
+            error: 'email and password are required'
+        });
+    }
+    next();
+}
+
+
+router
+
+    .get('/verify', ensureAuth, (req, res) => {
+        res.send({ valid: true });
+    })
+
+    .post('/signup', bodyParser, hasEmailAndPassword, (req, res, next) => {
+        const input = req.body;
+        delete req.body;
+
+        User.find({ email: input.email }).count()
+            .then(count => {
+                if(count > 0) throw {
+                    code: 400,
+                    error: `a user with email address ${input.email} already exists`
+                };
+
+                return new User(input).save();
+            })
+            .then(user => token.sign(user))
+            .then(token => res.send({ token }))
+            .catch(next);
+    })
+
+    .post('/signin', bodyParser, hasEmailAndPassword, (req, res, next) => {
+        const input = req.body;
+        delete req.body;
+
+        User.findOne({ email: input.email })
+            .then(user => {
+                if(!user || !user.comparePassword(input.password)) {
+                    throw {
+                        code: 400,
+                        error: 'invalid email address or password'
+                    };
+                }
+
+                return user;
+            })
+            .then(user => token.sign(user))
+            .then(token => res.send({ token }))
+            .catch(next);
+    });
+
+module.exports = router;
