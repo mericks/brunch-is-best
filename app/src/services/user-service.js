ec8186f8927a015f38d5a00fede3014d5535690a
mericks <mericks@users.noreply.github.com>
2017-05-08 18:34:57 -0700

    correct import notation on token; remove console logs

--- app/src/services/user-service.js
+++ app/src/services/user-service.js
@@ -1,15 +1,13 @@
-import http, { token } from './http';
+import http from './http';
+
 
 export default {
     getUser() {
-        console.log('inside getUser fn in user-svc');
         return http.fetch({
             path: '/user',
             method: 'GET',
-            token: token,
-        })
-        .then(user => console.log(user))
-        .catch(err => console.log(err));
+            token: http.token,
+        });
     },
 
 };
