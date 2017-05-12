55d0bbf6eb2b63a3499d4daea79e1055b9d1515a
mericks <mericks@users.noreply.github.com>
2017-05-12 09:04:31 -0700

    draft RestaurantReviews component for rendering

---
+++ app/src/components/restaurants/RestaurantReviews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const RestaurantReviews = props => {
+    props.reviews.map(review => {
+        return (
+            <div key={props.review._id}>
+                <h3>These are the reviews for the selected Restaurant</h3>
+                <p>Reviewed by: {review.reviewedBy}</p>
+                <p>Rating: {props.review.ratingValue} out of 5 stars. Date: {props.review.reviewDate}</p>
+                <p>{props.review.comment}</p>
+            </div>
+        );
+    }
+}
+
+export default RestaurantReviews;
