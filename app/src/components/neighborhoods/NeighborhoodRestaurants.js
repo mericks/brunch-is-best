2c1d7b950fa401f9aef3f6b20a0876d90afec31c
mericks <mericks@users.noreply.github.com>
2017-05-12 09:02:11 -0700

    remove fetcher and replace with restaurant-service; add AddRestaurantForm to view

--- app/src/components/neighborhoods/NeighborhoodRestaurants.js
+++ app/src/components/neighborhoods/NeighborhoodRestaurants.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import fetcher from '../../helpers/fetcher';
+import restaurantService from '../../services/restaurant-service';
 import NeighborhoodAllRestaurants from './NeighborhoodAllRestaurants';
 import RestaurantView from '../restaurants/RestaurantView';
+import AddRestaurantForm from '../forms/AddRestaurantForm';
 
 
 class NeighborhoodRestaurants extends Component {
@@ -14,30 +15,31 @@ class NeighborhoodRestaurants extends Component {
             // selectedQuadrant: 'View All',
             selectedView: '',
         };
-        this.fetchRestaurants = this.fetchRestaurants.bind(this);
         // this.updateNeighborhoods = this.updateNeighborhoods.bind(this);
         // this.updateView = this.updateView.bind(this);
     }
 
     componentDidMount() {
-        this.fetchRestaurants();
+        restaurantService.getAll()
+        .then(restaurants => this.setState({ restaurants }))
+        .then(() => this.updateView(this.state.selectedRestaurant))
+        .catch(err => console.log(err));
     }
 
-    fetchRestaurants() {
-        console.log('inside fetchRestaurants');
-        const brnchtkn = localStorage.getItem('brnchtkn');
-        console.log('token from local storage: ', brnchtkn);
-        fetcher({
-            path: '/restaurants',
-            method: 'GET',
-            // token: brnchtkn,
-        })
-        .then(restaurants => {
-            this.setState({ restaurants: restaurants });
-            this.updateView(this.state.selectedRestaurant);
-        });
+    // fetchRestaurants() {
+    //     fetcher({
+    //         path: '/restaurants',
+    //         method: 'GET',
+    //         // token: brnchtkn,
+    //     })
+    //     .then(restaurants => {
+    //         this.setState({ restaurants: restaurants });
+    //         this.updateView(this.state.selectedRestaurant);
+    //     });
 
-    }
+    // }
+
+
 
     // updateNeighborhoods(neighborhood) {
     //     let initialNeighborhoods = this.state.neighborhoods;
@@ -57,13 +59,14 @@ class NeighborhoodRestaurants extends Component {
     render() {
         return (
             <div>
-                <h3>This is Neighborhood Restaurants</h3>
+                <h3>These are restaurants in the selected neighborhood</h3>
                 <NeighborhoodAllRestaurants />
                 <RestaurantView /> 
+                <AddRestaurantForm />
             </div>
         );
     }
-}
+}   
 
 export default NeighborhoodRestaurants;
 
