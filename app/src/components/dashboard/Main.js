9fc63fb9bf41295b2181473975e8cbacb1f90bb3
mericks <mericks@users.noreply.github.com>
2017-05-16 23:10:31 -0700

    Main component with all nabe related components, updated state and fn bindings

--- app/src/components/dashboard/Main.js
+++ app/src/components/dashboard/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 import neighborhoodService from '../../services/neighborhood-service';
 import restaurantService from '../../services/restaurant-service';
 import QuadrantViewNav from '../neighborhoods/QuadrantViewNav';
@@ -7,6 +8,7 @@ import NeighborhoodsView from '../neighborhoods/NeighborhoodsView';
 import AddNeighborhoodForm from '../forms/AddNeighborhoodForm';
 import RestaurantsInNeighborhood from '../restaurants/RestaurantsInNeighborhood';
 import AddRestaurantForm from '../forms/AddRestaurantForm';
+import RestaurantView from '../restaurants/RestaurantView';
 
 
 class Main extends Component {
@@ -18,33 +20,42 @@ class Main extends Component {
             neighborhoodsInSelectedQuadrant: [],
 
             neighborhoods: [],
-            selectedNeighborhood: '',
+            selectedNeighborhood: {},
 
             restaurants: [],
             restaurantsInSelectedNeighborhood: [],
-            selectedRestaurant: '',
+            selectedRestaurant: {},
             
         };
-        this.updateNeighborhoods = this.updateNeighborhoods.bind(this);
-        this.updateQuadrantView = this.updateQuadrantView.bind(this);
+        this.addNeighborhood = this.addNeighborhood.bind(this);
+        this.addRestaurant = this.addRestaurant.bind(this);
+        this.updateNeighborhoodsInQuadrantView = this.updateNeighborhoodsInQuadrantView.bind(this);
+        this.updateRestaurantsInNeighborhoodView = this.updateRestaurantsInNeighborhoodView.bind(this);
+        this.updateSelectedRestaurant = this.updateSelectedRestaurant.bind(this);
     }
 
     componentDidMount() {
         neighborhoodService.getAll()
             .then(neighborhoods => this.setState({ neighborhoods }))
-            .then(() => this.updateQuadrantView(this.state.selectedQuadrant));
+            .then(() => this.updateNeighborhoodsInQuadrantView(this.state.selectedQuadrant));
         restaurantService.getAll()
             .then(restaurants => this.setState({ restaurants }));
     }
 
 
-    updateNeighborhoods(neighborhood) {
+    addNeighborhood(neighborhood) {
         let initialNeighborhoods = this.state.neighborhoods;
         let updatedNeighborhoods = [ ...initialNeighborhoods, neighborhood];
         this.setState({ neighborhoods: updatedNeighborhoods });
     }
 
-    updateQuadrantView(quadrant) {
+    addRestaurant(restaurant) {
+        let initialRestaurants = this.state.restaurants;
+        let updatedRestaurants = [ ...initialRestaurants, restaurant];
+        this.setState({ restaurants: updatedRestaurants });
+    }
+
+    updateNeighborhoodsInQuadrantView(quadrant) {
         this.setState({ selectedQuadrant: quadrant });
         if(quadrant === 'View All') {
             this.setState({ neighborhoodsInSelectedQuadrant: this.state.neighborhoods })
@@ -56,25 +67,42 @@ class Main extends Component {
         }
     }
 
-    updateNeighborhoodView(neighborhood) {
+    updateRestaurantsInNeighborhoodView(neighborhood) {
+        console.log('passedinnabe from updateRestoinNabe: ', neighborhood);
+        console.log('restaurants from updateRestoinNabe: ', this.state.restaurants);
         this.setState({ selectedNeighborhood: neighborhood });
         let updatedView = this.state.restaurants.filter(restaurant => {
-            return restaurant.neighborhood === neighborhood.name
+            return restaurant.neighborhood.name === neighborhood.name
         })
-        this.setState({ restaurantsInSelectedNeighborhood: updatedView })
-
+        this.setState({ restaurantsInSelectedNeighborhood: updatedView });
+        console.log('restaurantsInSelectedNeighborhood: ', this.state.restaurantsInSelectedNeighborhood);
     }
 
+    updateSelectedRestaurant(restaurant) {
+        this.setState({ selectedRestaurant: restaurant});
+    }
 
     render() {
         return (
             <div>
-                <QuadrantViewNav quadrants={this.state.quadrants} updateQuadrantView={this.updateQuadrantView}/>
-                <NeighborhoodsView neighborhoodsInSelectedQuadrant={this.state.neighborhoodsInSelectedQuadrant} updateNeighborhoodView={this.updateNeighborhoodView}  />
-                <AddNeighborhoodForm quadrants={this.state.quadrants} updateNeighborhoods={this.updateNeighborhoods}/>
-                <RestaurantsInNeighborhood restaurantsInSelectedNeighborhood={this.state.restaurantsInSelectedNeighborhood} />
-                <AddRestaurantForm selectedNeighborhood={this.state.selectedNeighborhood} neighborhoods={this.state.neighborhoods}/>
-                {/*<NeighborhoodRestaurants />*/}
+                <QuadrantViewNav
+                    quadrants={this.state.quadrants}
+                    updateNeighborhoodsInQuadrantView={this.updateNeighborhoodsInQuadrantView}/>
+                <NeighborhoodsView
+                    neighborhoodsInSelectedQuadrant={this.state.neighborhoodsInSelectedQuadrant}
+                    updateRestaurantsInNeighborhoodView={this.updateRestaurantsInNeighborhoodView}  />
+                <AddNeighborhoodForm
+                    quadrants={this.state.quadrants}
+                    addNeighborhood={this.addNeighborhood}/>
+                <RestaurantsInNeighborhood
+                    restaurantsInSelectedNeighborhood={this.state.restaurantsInSelectedNeighborhood}
+                    updateSelectedRestaurant={this.updateSelectedRestaurant} />
+                <AddRestaurantForm
+                    selectedNeighborhood={this.state.selectedNeighborhood}
+                    neighborhoods={this.state.neighborhoods}
+                    addRestaurant={this.addRestaurant} />
+                <RestaurantView 
+                    selectedRestaurant={this.state.selectedRestaurant} />
             </div>
         );
     }
