651b505edf02b49afeee5fe34ad9d3eebe988bef
mericks <mericks@users.noreply.github.com>
2017-05-22 22:04:15 -0700

    add signout button to nav

--- app/src/components/dashboard/Nav.js
+++ app/src/components/dashboard/Nav.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Nav = props => (
     <div>
         <h3>NAV: Welcome {props.userFirstName}.</h3>
+        <button onClick={() => props.handleSignOut()}>Sign out</button>
     </div>
 );
 
