b72ad2777d0f0f1389054b34e5ba906984c76f83
mericks <mericks@users.noreply.github.com>
2017-06-05 12:20:53 -0700

    early draft restaurants routes and tests

--- server/lib/routes/restaurants.js
+++ server/lib/routes/restaurants.js
@@ -1,24 +1,54 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser').json();
 const Restaurant = require('../models/restaurant');
+const Review = require('../models/review');
 
 router
 
     .get('/', (req, res, next) => {
         Restaurant.find().lean()
+            .populate('neighborhood createdBy')
             .then(restaurants => res.send(restaurants))
             .catch(next);
     })
 
     .get('/:id', (req, res, next) => {
         Restaurant.findById(req.params.id).lean()
+            .populate('address createdBy')
             .then(restaurant => res.send(restaurant))
             .catch(next);
     })
 
     .post('/', bodyParser, (req, res, next) => {
+        req.body['createdBy'] = req.user.id;
         new Restaurant(req.body).save()
             .then(restaurant => res.send(restaurant))
+            .catch(next);
+    })
+
+    .delete('/:id', (req, res, next) => {
+        const userID = req.user.id;
+        const restaurantID = req.params.id;
+        Promise.all([
+            Restaurant.findById(restaurantID).lean(),
+            Review.find({ restaurant: restaurantID }).lean(),
+        ])
+            .then(results => {
+                const restaurant = results[0];
+                const restaurantReviews = results[1];
+                if (userID !== restaurant.createdBy) {
+                    throw {
+                        code: 401,
+                        error: 'user not authorized to delete this restaurant',
+                    };
+                } else {
+                    restaurant.remove();
+                    restaurantReviews.remove();
+                }
+            })
+            .then(response => {
+                res.send({ removed: !!response });
+            })
             .catch(next);
     });
 
---
+++ server/test/api/restaurants.test.js
@@ -0,0 +1,82 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = chai.assert;
+
+const app = require('../../lib/app');
+const request = chai.request(app);
+
+
+
+describe.only('Restaurants API Route Tests', () => {
+
+    const restoUser = { name: { first: 'resto', last: 'user' }, email: 'resto@test.com', password: 'pword' };
+    let token = '';
+    let restoNabeID = '';
+    let restoUserID = '';
+
+    before(() => {
+        return request.post('/api/auth/signup')
+            .send(restoUser)
+            .then(res => {
+                token = res.body.token;
+            })
+            .then(() => {
+                return request.post('/api/neighborhoods')
+                    .send({ name: 'restoNabe', quadrant: 'SW' })
+                    .set('Authorization', token)
+                    .then(res => res.body)
+                    .then(savedNabe => {
+                        restoNabeID = savedNabe._id;
+                        restoUserID = savedNabe.createdBy;
+                    });
+            });
+    });
+
+    // function saveResto(resto) {
+    //     return request.post('/api/neighborhoods')
+    //         .send(resto)
+    //         .set('Authorization', token)
+    //         .then(res => res.body);
+    // }  
+ 
+
+    it('GET /restaurants returns an empty array', () => {
+        return request.get('/api/restaurants')
+            .set('Authorization', token)
+            .then(res => res.body)
+            .then(restaurants => assert.deepEqual(restaurants, []));
+    });
+
+    it('POST /restaurants - Adds restaurant per Restaurant Schema', () => {
+
+        let restoTest1 = {
+            name: 'restoTest1',
+            neighborhood: restoNabeID,
+            address: {
+                street: 'restoTest1Street',
+                city: 'restoTest1City',
+                zip: 12345,
+            },
+        };
+        
+        return request.post('/api/restaurants')
+            .send(restoTest1)
+            .set('Authorization', token)
+            .then(res => res.body)
+            .then(savedResto => {
+                console.log('savedResto: ', savedResto);
+                assert.ok(savedResto._id);
+                restoTest1._id = savedResto._id;
+                assert.equal(savedResto.name, restoTest1.name);
+                assert.equal(savedResto.neighborhood, restoTest1.neighborhood);
+                assert.equal(savedResto.address.street, restoTest1.address.street);
+                assert.equal(savedResto.address.city, restoTest1.address.city);
+                assert.equal(savedResto.address.zip, restoTest1.address.zip);
+                assert.equal(savedResto.createdBy, restoUserID);
+                // assert.equal(savedResto.createdBy, restoTest1.createdBy);
+            });
+    });
+
+
+});
