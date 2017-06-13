a8c25b0fe8b646a85f0ad8698da961f6a19294b9
mericks <mericks@users.noreply.github.com>
2017-06-13 11:15:33 -0700

    correct props/state ref

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import SignInForm from './forms/SignInForm';
 
 const Header = props => (
-    this.state.signedIn ? (
+    props.signedIn ? (
         <div>
             <h3>Welcome {props.userFirstName}.</h3>
             <button onClick={() => props.handleSignOut()}>Sign out</button>
