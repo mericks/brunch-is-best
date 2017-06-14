a55c6390506f1a49cfbedddb516d8dcace556d10
mericks <mericks@users.noreply.github.com>
2017-06-14 12:40:41 -0700

    remove unused import Route

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = props => {
-    return (
-        <div>
-            <h1>Brunch is Best!</h1>
-            <p>Don't have an account? Sign up now!</p>
-            <Link to={'/signup'}><button >Create an Account</button></Link>
-        </div>
-    );
-};
+
+const Home = props => (
+    <div>
+        <h1>Brunch is Best!</h1>
+        <p>Don't have an account? Sign up now!</p>
+        <Link to={'/signup'}><button >Create an Account</button></Link>
+    </div>
+);
 
 export default Home;
-
