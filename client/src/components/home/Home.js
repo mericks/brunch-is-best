78c361a190689424cc7ce6c77e9add5e676d82d6
Michelle Erickson <mericks@users.noreply.github.com>
2018-11-07 12:47:11 -0800

    Move landing page content to Home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import TitleEntry from './TitleEntry';
 
+class Home extends Component {
+    state = {
+        x: 0,
+        y: 0
+    };
 
-const Home = props => (
-    <div>
-        <h1>BRUNCHDAY</h1>
-        <p>Don't have an account? Sign up now!</p>
-        <Link to={'/signup'}><button >Create an Account</button></Link>
-    </div>
-);
+    // credit: Gokhan Demirhan https://codepen.io/gokhandemirhan/pen/WObNOm
+    componentWillMount() {
+        let w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth,
+            y = w.innerHeight || e.clientHeight || g.clientHeight;
+
+        this.setState({ x: x, y: y });
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <img className='bg' style={{ width: this.state.x+'px', height: this.state.y+'px' }} /> */}
+                <img
+                    className="bg"
+                    src={
+                        'https://source.unsplash.com/' +
+                        this.state.x +
+                        'x' +
+                        this.state.y +
+                        '/?brunch'
+                    }
+                />
+
+                <TitleEntry />
+            </div>
+        );
+    }
+}
 
 export default Home;
