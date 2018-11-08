816321ac042a5ac76631a57744686b61ee5aff6a
Michelle Erickson <mericks@users.noreply.github.com>
2018-11-08 13:13:32 -0800

    Convert TitleEntry to stateless component

--- client/src/components/home/TitleEntry.js
+++ client/src/components/home/TitleEntry.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import styles from './TitleEntry.css';
 import SignInForm from '../forms/SignInForm';
 
-class TitleEntry extends Component {
-    render() {
-        return (
-            <main className={styles.titleEntry}>
-                <div className="title">
-                    <strong>Brunchday!</strong>
-                </div>
-                <div className="signIn">
-                    <SignInForm />
-                </div>
-            </main>
-        );
-    }
-}
+const TitleEntry = props => (
+    <main className={styles.titleEntry}>
+        <div className="title">
+            <strong>Brunchday!</strong>
+        </div>
+        <SignInForm {...props} handleSignIn={props.handleSignIn} />
+    </main>
+);
 
 export default TitleEntry;
