a7a5e6e1aa4c141b5c2a54e2b488f36c3ef185fa
Michelle Erickson <mericks@users.noreply.github.com>
2018-11-08 13:12:34 -0800

    Clean up spacing / housekeeping

--- client/src/services/auth-service.js
+++ client/src/services/auth-service.js
@@ -6,34 +6,35 @@ function setToken(token) {
 }
 
 export default {
-
     verify(token) {
-        return http.fetch({
-            path: '/auth/verify',
-            method: 'GET',
-            token,
-        })
-        .catch(err => console.log(err));
+        return http
+            .fetch({
+                path: '/auth/verify',
+                method: 'GET',
+                token
+            })
+            .catch(err => console.log(err));
     },
 
     signup(formPayload) {
-        return http.fetch({
-            path: '/auth/signup',
-            method: 'POST',
-            body: formPayload,
-        })
-        .then(({ token }) => setToken(token))
-        .catch(err => console.log(err));
+        return http
+            .fetch({
+                path: '/auth/signup',
+                method: 'POST',
+                body: formPayload
+            })
+            .then(({ token }) => setToken(token))
+            .catch(err => console.log(err));
     },
 
     signin(formPayload) {
-        return http.fetch({
-            path: '/auth/signin',
-            method: 'POST',
-            body: formPayload,
-        })
-        .then(({ token }) => setToken(token))
-        .catch(err => console.log(err));
+        return http
+            .fetch({
+                path: '/auth/signin',
+                method: 'POST',
+                body: formPayload
+            })
+            .then(({ token }) => setToken(token))
+            .catch(err => console.log(err));
     }
-    
 };
